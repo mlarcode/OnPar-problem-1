eb3197fc255b1e2591b12efb9ead2dba16807605
mlarcode <sightsnsenses@gmail.com>
2016-07-21 21:02:36 -0400

    first commit

---
+++ OnPar Problem 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnPar_Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter a two digit number
+            int num, sum = 0, r;
+            Console.WriteLine("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                r = num % 10;
+                num = num / 10;
+                sum = sum + r;
+            }
+            Console.WriteLine("Sum of Digits of the Number : " + sum);
+            Console.Write("Do you want to enter another number?(YES/No):" + "\n");
+            char y = (char)Console.Read();
+
+            if (Char.IsLetter(y))
+            {
+                if (Char.IsUpper(y))
+                {
+                    Console.WriteLine("Continue");
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye:");
+                }
+            }
+        }
+              
+
+                       
+
+                                  
+                    
+                    
+
+                    
+                    
+
+         
+                    
+
+                    
+                
+       
+
+   
